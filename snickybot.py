695110e6dfe19f1806c38f8b5b5c952448f362dd
Nicky Ringland <nickyringland@gmail.com>
2018-07-26 23:32:13 +1000

    tutor roster bot now runs continuously, updates if next tutor slot changes, and reminds tutor if startin in next 5 mins.

--- snickybot.py
+++ snickybot.py
@@ -1,12 +1,13 @@
 from slackclient import SlackClient
 from icalevents import icalevents
 from datetime import datetime, timezone, timedelta
+import time
 import os
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 url = 'https://calendar.google.com/calendar/ical/ncss.edu.au_7hiaq9tlca1lsgfksjss4ejc4s%40group.calendar.google.com/private-23775cab8b8397efb35dd7f2b6e67d84/basic.ics'
-
+channel="CBXDYDGFP"
 
 def get_next_tutor(now):
   evs = icalevents.events(url=url)
@@ -18,10 +19,34 @@ def get_next_tutor(now):
       #this is the first one in the future
       return(ev)
 
-now = datetime.now(timezone.utc)
-next_tutor = get_next_tutor(now)
-sc.api_call(
-  "chat.postMessage",
-  channel="CBXDYDGFP",
-  text=":smile: Next tutor {} in {}".format(next_tutor.summary, (-(now - next_tutor.start)))
-)
+def event_is_same(ev1, ev2):
+  if not ev1 or not ev2:
+    return ev1 == ev2
+  return ev1.uid == ev2.uid
+
+
+announced_next_tutor = None
+while True:
+  now = datetime.now(timezone.utc)
+  next_tutor = get_next_tutor(now)
+  impending_tutor_time = -(now - next_tutor.start)
+  if not event_is_same(next_tutor, announced_next_tutor):
+    sc.api_call(
+      "chat.postMessage",
+      channel=channel,
+      text=":smile: Next tutor {} in {}".format(next_tutor.summary, (impending_tutor_time))
+    )
+    announced_next_tutor = next_tutor
+  print(f'checking ical next tutor {announced_next_tutor.summary}')
+  if impending_tutor_time.total_seconds() < (5*60):
+    sc.api_call(
+      "chat.postMessage",
+      channel=channel,
+      text="remind {} starting soon! plz ack".format(next_tutor.summary)
+    )
+
+  time.sleep(60)
+  #time.sleep(td.total_seconds())
+
+
+
